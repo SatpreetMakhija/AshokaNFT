82816e71e89bdd6e85df2d35d96a88ca2cc2523a
SatpreetMakhija <sm.satpreet@gmail.com>
2022-05-03 07:48:37 +0530

    created transaction in mint-nft.js

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,5 +1,8 @@
 require("dotenv").config()
 const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
@@ -7,3 +10,20 @@ const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 const contractAddress = "0x53933E3A409476Ce9ddd9B20c1401581f3c334A5";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest');
+    //get the latest nonce (transaction count of EOA)
+
+    //the transaction
+    const tx = {
+        'from': PUBLIC_KEY,
+        'to': contractAddress,
+        'nonce': nonce,
+        'gas': 500000,
+        'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+
+
+}
+
